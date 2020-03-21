b9ded6053d75da99034901506c5fd1c0075592c2
Andre Herculano <andresilveirah@gmail.com>
2020-03-21 13:48:10 +0100

    twosdks tests

--- Example/CCPAConsentViewController/ViewController.swift
+++ Example/CCPAConsentViewController/ViewController.swift
@@ -8,22 +8,38 @@
 
 import UIKit
 import CCPAConsentViewController
+import ConsentViewController
 
-class ViewController: UIViewController, ConsentDelegate {
+class ViewController: UIViewController, ConsentDelegate, GDPRConsentDelegate {
     
     let logger = Logger()
 
     lazy var consentViewController: CCPAConsentViewController = { return CCPAConsentViewController(
-        accountId: 22,
-        propertyId: 6099,
-        propertyName: try! PropertyName("ccpa.mobile.demo"),
-        PMId: "5df9105bcf42027ce707bb43",
-        campaignEnv: .Public,
+        accountId: 378,
+        propertyId: 4074,
+        propertyName: try! PropertyName("vice.ios.app"),
+        PMId: "5e19063e6468c12231c899a8",
+        campaignEnv: .Stage,
+        targetingParams: ["SDK_TYPE":"CCPA"],
+        consentDelegate: self
+    )}()
+    
+    lazy var gdpr: GDPRConsentViewController = { return GDPRConsentViewController(
+        accountId: 378,
+        propertyId: 4074,
+        propertyName: try! GDPRPropertyName("vice.ios.app"),
+        PMId: "5e19063e6468c12231c899a8",
+        campaignEnv: .Stage,
+        targetingParams: ["SDK_TYPE":"GDPR"],
         consentDelegate: self
     )}()
     
     func ccpaConsentUIWillShow() {
         present(consentViewController, animated: true, completion: nil)
+    }
+    
+    func gdprConsentUIWillShow() {
+        present(gdpr, animated: true, completion: nil)
     }
 
     func consentUIDidDisappear() {
@@ -31,11 +47,18 @@ class ViewController: UIViewController, ConsentDelegate {
     }
     
     func onConsentReady(consentUUID: ConsentUUID, userConsent: UserConsent) {
+        print("-- CCPA --")
         print("consentUUID: \(consentUUID)")
         print("userConsents: \(userConsent)")
     }
+    
+    func onConsentReady(gdprUUID: GDPRUUID, userConsent: GDPRUserConsent) {
+        print("-- GDPR --")
+        print("gdprUUID: \(gdprUUID)")
+        print("userConsents: \(userConsent)")
+    }
 
-    func onError(error: CCPAConsentViewControllerError?) {
+    @nonobjc func onError(error: CCPAConsentViewControllerError?) {
         logger.log("Error: %{public}@", [error?.description ?? "Something Went Wrong"])
     }
 
@@ -46,6 +69,9 @@ class ViewController: UIViewController, ConsentDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         consentViewController.loadMessage()
+        gdpr.loadMessage()
     }
 }
 
+
+{"mmsCookies":["_sp_v1_uid=1:131:5a42671e-5320-467a-8f2c-f82efc5f3035;","_sp_v1_csv=1;","_sp_v1_lt=1:msg|true:;","_sp_v1_ss=1:H4sIAAAAAAAAAItWqo5RKimOUbKKBjLyQAyD2lidGKVUEDOvNCcHyC4BK6iurVWKBQAW54XRMAAAAA%3D%3D;","_sp_v1_opt=1:;","_sp_v1_data=2:89576:1584794536:0:1:0:1:0:0:27753693-5f8c-4da6-b1f5-adde4488b1e8:114806;"],"messageId":"114806","dnsDisplayed":true,"status":"consentedAll"}"
--- Example/Pods/Target Support Files/Pods-CCPAConsentViewController_Example/Pods-CCPAConsentViewController_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CCPAConsentViewController_Example/Pods-CCPAConsentViewController_Example-frameworks.sh
@@ -162,9 +162,11 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CCPAConsentViewController/CCPAConsentViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ConsentViewController/ConsentViewController.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/CCPAConsentViewController/CCPAConsentViewController.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/ConsentViewController/ConsentViewController.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
